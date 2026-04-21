7a2220ca09b0613f1125f68926597aa3de00930f
rejuanigg <riosjuan32223@gmail.com>
2026-04-21 16:06:34 -0300

    feat(templates):create product card template and add in app.vue, create nav.vue file

---
+++ fishing-store-frontend/src/App.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { ref } from 'vue';
+import ProductCard from './ProductCard.vue';
+import Nav from './Nav.vue';
+
+const products = ref([
+  {
+    name:"Caster Snipper",
+    section:"Cañas",
+    category:"Fibra de Vidrio",
+    description:"Its good",
+    score:"8.9",
+    price:9999,
+    image:2
+  },
+  {
+    name:"Caster Trigger",
+    section:"Cañas",
+    category:"Aluminio",
+    description:"Its sooooo goood",
+    score:"9.9",
+    price:999229,
+    image:2
+  },
+  {
+    name:"Caster Reel",
+    section:"Reel",
+    category:"Grafito",
+    description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque perspiciatis ",
+    score:"7.9",
+    price:123,
+    image:2
+  }
+]);
+
+
+</script>
+
+<template>
+
+<section class="hero">
+
+</section>
+
+<Nav></Nav>
+
+<ProductCard v-for="product in products" :product="product">
+
+</ProductCard>
+
+</template>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  list-style: none;
+  outline: none;
+}
+
+</style>
---
+++ fishing-store-frontend/src/ProductCard.vue
@@ -0,0 +1,90 @@
+<script setup>
+defineProps(['product'])
+</script>
+
+<template>
+
+  <div class="product--card">
+    <div class="product--card__header">
+      <div class="card--header__image">
+        {{ product.image }}
+      </div>
+    </div>
+    <div class="product--card__body">
+      <span class="product--name">{{ product.name }}</span>
+      <div class="card--body__metrics">
+        <span class="product--score">{{ product.score }} ⭐</span>
+        <span class="product--category">{{ product.category }}</span>
+      </div>
+      <span class="product--description">{{ product.description }}</span>
+    </div>
+    <div class="product--card__foot">
+      <span class="product--price">$ {{ product.price }}</span>
+      <button class="card--foot__add-button">Add</button>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+
+.product--card{
+  background-color: antiquewhite;
+  width: 300px;
+  height: 500px;
+  margin: 5px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  border-radius: 5px;
+}
+
+.product--card__header{
+  background-color: cornsilk;
+  height: 60%;
+  width: 100%;
+}
+
+.product--card__body{
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  padding: 10px;
+
+}
+
+.card--body__metrics{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 12px;
+}
+
+.product--card__foot{
+  display: flex;
+  justify-content: space-between;
+}
+
+.product--name{
+  font-weight: 600;
+  font-size: 20px;
+  margin-bottom: 10px;
+}
+
+.product--category{
+  background-color: aliceblue;
+  padding: 5px;
+  border-radius: 20px;
+}
+
+.product--price{
+  font-size: 20px;
+}
+
+.card--foot__add-button{
+  width: 40%;
+}
+
+</style>
