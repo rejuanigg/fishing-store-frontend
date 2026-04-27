3aaee42e283c17f46cd24b7443c956058083779f
rejuanigg <riosjuan32223@gmail.com>
2026-04-26 21:38:36 -0300

    feat(routing): add routing in navbar

--- fishing-store-frontend/src/components/Nav.vue
+++ fishing-store-frontend/src/components/Nav.vue
@@ -1,7 +1,27 @@
 <script setup>
+import CartView from '@/router/CartView.vue';
+import HomeView from '@/router/HomeView.vue';
+import ProductView from '@/router/ProductView.vue';
 import { cartStore } from '@/stores/cart';
+import { computed, ref } from 'vue';
 
 const cart = cartStore();
+
+const routes = {
+  '/': HomeView,
+  '/products': ProductView,
+  '/cart':CartView
+}
+
+const currentPath = ref(window.location.hash);
+
+window.addEventListener('hashchange', ()=>{
+  currentPath.value = window.location.hash;
+})
+
+const currentView = computed(()=>{
+  return routes[currentPath.value.slice(1)||'/|| NotFound']
+})
 
 </script>
 
@@ -11,14 +31,14 @@ const cart = cartStore();
 
   </img>
   <ul class="w-2/4 flex justify-between">
-    <li class="">Home</li>
-    <li class="">Products</li>
-    <li>//////</li>
-    <li>//////</li>
+    <a href="/">Home</a>
+    <a href="/products">Productos</a>
   </ul>
-  <div class="" >
+  <div class="">
+    <a href="/cart">Carrito</a>
     <span>{{ cart.count }}</span>
   </div>
 </div>
+<component is="currentView" />
 </template>
 
