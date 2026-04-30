fa69267e29ad19384b03b058335862c09088aa32
rejuanigg <riosjuan32223@gmail.com>
2026-04-30 13:38:24 -0300

    feat(admin panel):add create category and section view

--- fishing-store-frontend/src/views/admin-panel/CategoryPanelView.vue
+++ fishing-store-frontend/src/views/admin-panel/CategoryPanelView.vue
@@ -1,8 +1,60 @@
 <script setup>
+import { useCategory } from '@/composables/useCategory';
+import api from '@/services/api';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const {sections} = useCategory();
+const name = ref('');
+const sectionId = ref(null);
+
+async function onSubmit(){
+  const response = await api.post('/categories',{
+    name: name.value,
+    section_id: sectionId.value
+  })
+
+  return router.push('/admin-panel')
+}
 
 </script>
 
 
 <template>
-  <h1>Honduras</h1>
+    <div class="bg-red-50 w-full h-full flex justify-center items-center">
+      <form class="bg-red-300 w-4/5 h-120 p-10" @submit.prevent="onSubmit">
+        <div class="flex flex-col">
+
+        <div class="w-full flex justify-center h-1/5">
+          <h2 class="text-xl font-bold">Añade una Seccion</h2>
+        </div>
+
+
+          <div>
+            <div>
+
+              <div class="w-1/4">
+                <span>Seccion</span>
+                <select v-model="sectionId" >
+                  <option v-for="section in sections" :value="section.id">{{ section.name }}</option>
+                </select>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+
+        <div class="h-1/6 flex flex-col">
+          <label for="" class="font-semibold">Nombre</label>
+          <input v-model="name" type="text" name="sa" id="s" class="bg-white h-full">
+        </div>
+
+        <button class="h-1/6 font-semibold">
+          Add
+        </button>
+
+      </form>
+  </div>
 </template>
--- fishing-store-frontend/src/views/admin-panel/SectionPanelView.vue
+++ fishing-store-frontend/src/views/admin-panel/SectionPanelView.vue
@@ -1,9 +1,38 @@
-<script>
+<script setup>
+import api from '@/services/api';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const name = ref('');
+
+async function onSubmit() {
+  const response = await api.post('/sections', {
+    name: name.value
+  })
+  return router.push('/admin-panel')
+}
 
 </script>
 
 <template>
-  <div>
-    
+  <div class="bg-red-50 w-full h-full flex justify-center items-center">
+      <form @submit.prevent="onSubmit" class="bg-red-300 w-4/5 h-120 p-10">
+        
+        <div class="w-full flex justify-center h-1/5">
+          <h2 class="text-xl font-bold">Añade una Seccion</h2>
+        </div>
+
+
+        <div class="h-1/6 flex flex-col">
+          <label for="" class="font-semibold">Nombre</label>
+          <input v-model="name"  type="text" name="sa" id="s" class="bg-white h-full">
+        </div>
+
+        <button class="h-1/6 font-semibold">
+          Agregar Seccion
+        </button>
+
+      </form>
   </div>
 </template>
