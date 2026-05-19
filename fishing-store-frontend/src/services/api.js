f1cd1a63cce9ec32c7dbf0cbd6ef282168e561ca
rejuanigg <riosjuan32223@gmail.com>
2026-05-19 11:56:21 -0300

    chore: prepare frontend for vercel deploy

--- fishing-store-frontend/src/services/api.js
+++ fishing-store-frontend/src/services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
-
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api'
-})
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    Accept: 'application/json',
+  },
+});
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token')
@@ -12,7 +14,5 @@ api.interceptors.request.use((config) => {
     }
     return config
 })
-
-
 
 export default api
